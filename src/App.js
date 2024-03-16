cd434a0fced3cd5ec509581459e40f665dd6171f
Aniket Karande <aniketkarande27@gmail.com>
2024-03-17 00:42:00 +0530

    Code clean upp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Todo- app using Context</header>
+    <div >
+      <header>Todo- app using Context</header>
     </div>
   );
 }
